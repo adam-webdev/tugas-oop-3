1f9555ed8d7190779f0ae97a5bbe790fd29b9080
adam-webdev <adamdwimaulana2605@gmail.com>
2023-11-07 22:58:53 +0700

    oop-pertemuan6

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace tugas6{
+
+  public class Pegawai{
+    private string _nama;
+    private double _gajiPokok;
+
+    public void setNama(string paramNama){
+      _nama = paramNama;
+    }
+
+    public string getNama(){
+      return _nama;
+    }
+
+    public void setGajiPokok(double paramGaji){
+      _gajiPokok = paramGaji;
+    }
+
+    public double getGajiPokok(){
+      return _gajiPokok;
+    }
+
+    public virtual void cetakInfo(){
+      Console.WriteLine("Pegawai : " + getNama());
+      Console.WriteLine("Gaji Pokok : " + getGajiPokok());
+    }
+  }
+
+  public class Manager : Pegawai{
+    private double _tunjangan;
+
+    public void setTunjangan(double paramTunjangan){
+      _tunjangan = paramTunjangan;
+    }
+
+    public double getTunjangan(){
+      return _tunjangan;
+    }
+
+    public void cetakTunjangan(){
+      Console.WriteLine("Tunjangan Anda : " + getTunjangan());
+    }
+     public virtual void cetakInfo(){
+      Console.WriteLine("Pegawai : " + getNama());
+      Console.WriteLine("Posisi anda : Manager" );
+      Console.WriteLine("Gaji Pokok Anda : " + getGajiPokok());
+    }
+  }
+
+  public class Programmer : Pegawai{
+    private double _bonus;
+
+    public void setBonus(double paramBonus){
+      _bonus = paramBonus;
+    }
+
+    public double getBonus(){
+      return _bonus;
+    }
+
+    public void cetakBonus(){
+      Console.WriteLine("Bonus Anda : " + getBonus());
+    }
+
+    public virtual void cetakInfo(){
+      Console.WriteLine("Pegawai : " + getNama());
+      Console.WriteLine("Posisi : Programmer" );
+      Console.WriteLine("Gaji Pokok Anda : " + getGajiPokok());
+    }
+
+  }
+
+  class Program{
+    static void Main(string[] args){
+      Console.WriteLine("=========Pegawai Class==========");
+      Pegawai pegawai = new Pegawai();
+      pegawai.setNama("Adam Pegawai");
+      pegawai.setGajiPokok(1500000);
+      pegawai.cetakInfo();
+
+      Console.WriteLine("=========Manager Class==========");
+      Manager manager = new Manager();
+      manager.setNama("Adam Manager");
+      manager.setGajiPokok(25000000);
+      manager.setTunjangan(2000000);
+      manager.cetakInfo();
+      manager.cetakTunjangan();
+
+      Console.WriteLine("=========Programmer Class==========");
+      Programmer programmer = new Programmer();
+      programmer.setNama("Adam Programmer");
+      programmer.setGajiPokok(20000000);
+      programmer.setBonus(1200000);
+      programmer.cetakInfo();
+      programmer.cetakBonus();
+    }
+  }
+
+}
